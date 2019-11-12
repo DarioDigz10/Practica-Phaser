d5dbf07c1d776205ec0e80670bbf4a74cbe14bcf
DarioDigz10 <46105884+DarioDigz10@users.noreply.github.com>
2019-11-13 00:14:09 +0100

    Cambio a los controles y sprites de barriles nuevos.

--- JS/MainMenu.js
+++ JS/MainMenu.js
@@ -1,49 +1,49 @@
 class MainMenu extends Phaser.Scene {
 
-                constructor() {
-                    super("MainMenu");
-                }
-                preload()
-                {
-                    this.load.image('fondo', 'assets/fondoMenu.jpg');
-					this.load.image('escenario', 'assets/escenario.jpg');
-					this.load.image('logoJuego', 'assets/logoWaveJump.png');
+    constructor() {
+        super("MainMenu");
+    }
+    preload()
+    {
+        this.load.image('fondo', 'assets/fondoMenu.jpg');
+        this.load.image('escenario', 'assets/escenario.jpg');
+        this.load.image('logoJuego', 'assets/logoWaveJump.png');
 
-                }
+    }
 
-                create()
-                {	
-					this.add.image(750, 450, 'fondo').setScale(2.07);					
-					
-					
-					
-                    var texto = this.add.text(game.config.width / 2, (game.config.height / 2) + 200, 'Press Enter to start.', {
-                        fontSize: '40px',
-                        fill: '#ffffff',
-						stroke :  '#00000' , 
-						strokeThickness :  3 ,
-						fontFamily: 'Amatica SC'
-                    }).setOrigin(0.5)
-					
-					this.logoMovil = this.add.image(game.config.width / 2, (game.config.height / 2) - 30, 'logoJuego').setScale(0.2);
-					this.speed = 0.3;
-					
-                }
-	
-                update()
-                {
-                    if (this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.ENTER).isDown) {
-                        this.scene.start("EscenaJuego");
-                    }
-					
-					this.logoMovil.y += this.speed;
-					//console.log(this.logoMovil.y);
-					if(this.logoMovil.y >= ((game.config.height / 2)) || this.logoMovil.y <= ((game.config.height / 2) - 30))
-					{
-						this.speed *= -1;
-						//console.log(this.speed);
-					}
-					
-                }
+    create()
+    {
+        this.add.image(750, 450, 'fondo').setScale(2.07);
 
-            }
+
+
+        var texto = this.add.text(game.config.width / 2, (game.config.height / 2) + 200, 'Press Enter to start.', {
+            fontSize: '40px',
+            fill: '#ffffff',
+            stroke: '#00000',
+            strokeThickness: 3,
+            fontFamily: 'Amatica SC'
+        }).setOrigin(0.5)
+
+        this.logoMovil = this.add.image(game.config.width / 2, (game.config.height / 2) - 30, 'logoJuego').setScale(0.2);
+        this.speed = 0.3;
+
+    }
+
+    update()
+    {
+        if (this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.ENTER).isDown) {
+            this.scene.start("EscenaJuego");
+        }
+
+        this.logoMovil.y += this.speed;
+        //console.log(this.logoMovil.y);
+        if (this.logoMovil.y >= ((game.config.height / 2)) || this.logoMovil.y <= ((game.config.height / 2) - 30))
+        {
+            this.speed *= -1;
+            //console.log(this.speed);
+        }
+
+    }
+
+}
--- JS/Player.js
+++ JS/Player.js
@@ -1,21 +1,20 @@
 
 class Player {
-    constructor(x, y, scene) {
-        this.object = scene.physics.add.sprite(x, y, 'dude');
-		this.object.setDepth(1);
+    constructor(x, y, sprite, scene) {
+        this.object = scene.physics.add.sprite(x, y, sprite);
+        this.object.setDepth(1);
         this.object.setBounce(0);
-        this.object.setScale(2);
         scene.physics.add.collider(this.object, colliders);
         //Puntero al componente body:
         this.body = this.object.body;
         //Puntero al componente body.velocity:
-        this.velocity =  this.body.velocity;
-		this.muerto=false;
+        this.velocity = this.body.velocity;
+        this.muerto = false;
     }
-	matar(){
-		
-		 //this.object.disableBody(true, true);
-		 this.muerto=true;
-		 this.object.destroy();
-	}
+    matar() {
+
+        //this.object.disableBody(true, true);
+        this.muerto = true;
+        this.object.destroy();
+    }
 }
